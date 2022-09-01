28b7c92eb638000c4fb643f68c20267857e268b6
leowipin <leonardopincayjaramillo@gmail.com>
2022-09-01 17:28:04 -0500

    mostrado dinamico de las categorias

--- proyecto 3/frontend/src/app/interfaz/producto.ts
+++ proyecto 3/frontend/src/app/interfaz/producto.ts
@@ -1,6 +1,7 @@
 export interface Producto {
     product_id:number;
     name:string;
+    category:string;
     quantity_in_stock:number;
     unit_price:number;
     picture:string;
--- proyecto 3/frontend/src/app/menu-inicial/menu-inicial.component.ts
+++ proyecto 3/frontend/src/app/menu-inicial/menu-inicial.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoService } from '../servicios/producto.service';
 import { Producto } from '../interfaz/producto';
+import { timeout } from 'rxjs';
 @Component({
   selector: 'app-menu-inicial',
   templateUrl: './menu-inicial.component.html',
@@ -9,13 +10,27 @@ import { Producto } from '../interfaz/producto';
 export class MenuInicialComponent implements OnInit {
 
   productos: Producto[] = [];
+  categorias: string[] = [];
 
-  constructor(private productoService:ProductoService) {
-   }
+  constructor(private productoService: ProductoService) {
+  }
   ngOnInit(): void {
+    this.inicializarProductos();
+  }
+  inicializarProductos() {
     this.productoService.obtenerProductos().subscribe(respuesta => {
       this.productos = respuesta as Producto[];
+      this.inicializarCategorias(this.productos);
     })
+
+  }
+  inicializarCategorias(prods: Producto[]) {
+    for (let producto of prods) {
+      let categoria = producto.category;
+      if (!this.categorias.includes(categoria)) {
+        this.categorias.push(categoria)
+      }
+    }
   }
 
 }
